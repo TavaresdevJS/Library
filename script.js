5ee6194228dcc564cc13ac5a30b6f135a0d9862b
TavaresdevJS <178415368+TavaresdevJS@users.noreply.github.com>
2026-08-12 16:43:20 -0300

    Add addBookToLibrary function

--- script.js
+++ script.js
@@ -8,4 +8,10 @@ function Book(title, author, pages, read) {
   this.id = crypto.randomUUID();
 }
 
-function addBookToLibrary() {}
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+}
+
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 310, true);
+console.log(myLibrary);
